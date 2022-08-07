1caa8a3d2ef323c344ff5b5a0bfb6320c61ef392
Ahmed Mohammed <ahmed.mohd200130@gmail.com>
2022-08-07 20:24:36 +0200

    complete homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import Image from 'next/image'
 import { Fragment } from 'react'
-import Navbar from '../components/Navbar'
-import styles from '../styles/Home.module.scss'
 import Main from './main'
 
 export default function Home() {
